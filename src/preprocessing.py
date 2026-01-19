bd6e8e31f999f6820a3a9431fd14d85b301bd1cb
Sumeeth HB <sumeethofficial@gmail.com>
2026-01-20 01:56:23 +0530

    Create preprocessing.py

---
+++ src/preprocessing.py
@@ -0,0 +1,11 @@
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import SMOTE
+
+def preprocess(X, y):
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    smote = SMOTE(random_state=42)
+    X_resampled, y_resampled = smote.fit_resample(X_scaled, y)
+
+    return X_resampled, y_resampled
